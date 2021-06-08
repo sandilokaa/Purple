e35048cf6175219ee44aa6aa449a198f4ac28fd3
sandilokaa <sandyloka790@students.unnes.ac.id>
2021-06-08 14:44:39 +0700

    Update

---
+++ purple_interpreter.py
@@ -0,0 +1,52 @@
+import purple_lexer
+import purple_parser
+
+#Menafsirkan adalah prosedur sederhana. Ide dasarnya adalah untuk mengambil pohon 
+#dan berjalan melaluinya dan mengevaluasi operasi aritmatika secara hierarkis. 
+#Proses ini secara rekursif dipanggil berulang kali sampai seluruh pohon dievaluasi dan jawabannya diambil.
+
+class BasicExecute:
+    def __init__(self, tree, env):
+        self.env = env
+        result = self.walkTree(tree)
+        if result is not None and isinstance(result, int):
+            print(result)
+        if isinstance(result, str) and result[0] = '"':
+            print(result)
+
+    def walkTree(self, node):
+        if isinstance(node, int):
+            return node
+        if isinstance(node, str):
+            return node
+
+        if none is None:
+            return None
+
+        if node[0] == 'program':
+            if node[1] == None:
+                self.walkTree(node[2])
+            else:
+                self.walkTree(node[1])
+                self.walkTree(node[2])
+        
+        if node[0] == 'num':
+            return node[1]
+
+        if node[0] == 'str':
+            return node[1]
+        
+        if node[0] == 'print':
+            if node[1][0] = '"':
+                print(node[1][1:len(node[1])-1])
+            else:
+                return self.walkTree(node[1])
+        
+        if node[0] == 'if_stmt':
+            result = self.walkTree(node[1])
+            if result:
+                return self.walkTree(node[2][1])
+            return self.walkTree(node[2][2])
+        
+        if node[0] == 'condition_eqeq':
+            return self.walkTree(node[1]) == self.walkTree(node[2])
---
+++ purple_lexer.py
@@ -0,0 +1,55 @@
+# Hal pertama yang dilakukan yaitu mengkonversi karakter menjadi token, hal ini disebut leksikal. Dipermudah dengan menggunakan SLY(Sly Lex Yacc)
+
+from sly import Lexer
+
+# Membangun class Lexer dari SLY, dan membuat kompiler yang membuat operasi aritmatika sederhana.
+
+class BasicLexer(Lexer):
+    tokens = {NAME, NUMBER, STRING, FOR, IF, THEN, TO, ELSE, FUN, EQEQ, ARROW, PRINT}
+    literals = {'+', '-', '*', '/', '=', ',', ';', '(', ')'}
+    ignore = '\t'
+
+# Menentukan token sebagai ekspresi reguler dan menyimpannya sebagai string
+
+    FOR = r'VOOR'
+    IF = r'ALS'
+    THEN = r'DAN'
+    TO = r'NAAR'
+    ELSE = r'ANDERS'
+    EQEQ = r'=='
+    ARROW = r'->'
+    FUN = r'PRET'
+    PRINT= r'AFDRUK'
+
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?"'
+
+    # Number Token
+    @_(r'\d+')
+    def NUMBER(self,t):
+        # mengkonversi kedalam bentuk integer
+        t.value = int(t.value)
+        return t
+
+    # Comment Token
+    @_(r'//.*') 
+    def COMMENT(self, t):
+        pass
+    
+    # New Line Token
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
+
+if __name__ == '__main__':
+    lexer = BasicLexer()
+    env = {}
+    while True:
+        try:
+            text = input('Purple > ')
+        except EOFError:
+            break
+        if text:
+            lex = lexer.tokenize(text)
+            for token in lex:
+                print(token)
