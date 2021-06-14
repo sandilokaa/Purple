84ce0b76b6777104bd68cf5c8b59b64a4614ecc9
sandilokaa <sandyloka790@students.unnes.ac.id>
2021-06-14 13:16:45 +0700

    Finish main

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import purple_interpreter
 
 from sys import *
 
+lexer = purple_lexer.PurpleLexer()
+parser = purple_parser.PurpleParser()
+env = {}
+
+file = open.(argv[1])
+text = file.readlines()
+for line in text:
+    tree = parser.parse(lexer.tokenize(line))
+    purple_interpreter.PurpleExecute(tree, env)
